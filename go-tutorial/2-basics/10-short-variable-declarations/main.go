534c507b7f83554bc1665c511c69f97616a48124
Ido <mukae.yuta.0811@gmail.com>
2025-10-21 17:13:02 +0900

    Basics 11/17まで

---
+++ go-tutorial/2-basics/10-short-variable-declarations/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+		var i, j int = 1, 2
+		k := 3 // :=は関数内でのみ利用可能, :=は自動で型推論してくれる・varが必要ない
+		c, python, java := true, false, "no!" // 複数定義することも可能
+
+		fmt.Println(i, j, k, c, python, java) // 1 2 3 true false no!
+}
